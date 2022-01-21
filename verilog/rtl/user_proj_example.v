fb1ff7079459c5696a72f8cc2930cadc5cd43190
mdzakirhussain <zakir.hussain.work@gmail.com>
2022-01-21 16:45:04 +0530

    Initial commit

---
+++ verilog/rtl/user_proj_example.v
@@ -0,0 +1,119 @@
+// SPDX-FileCopyrightText: 2020 Efabless Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// SPDX-License-Identifier: Apache-2.0
+
+`default_nettype none
+/*
+ *-------------------------------------------------------------
+ *
+ * user_proj_example
+ *
+ * This is an example of a (trivially simple) user project,
+ * showing how the user project can connect to the logic
+ * analyzer, the wishbone bus, and the I/O pads.
+ *
+ * This project generates an integer count, which is output
+ * on the user area GPIO pads (digital output only).  The
+ * wishbone connection allows the project to be controlled
+ * (start and stop) from the management SoC program.
+ *
+ * See the testbenches in directory "mprj_counter" for the
+ * example programs that drive this user project.  The three
+ * testbenches are "io_ports", "la_test1", and "la_test2".
+ *
+ *-------------------------------------------------------------
+ */
+
+module user_proj_example #(
+    parameter BITS = 32
+)(
+`ifdef USE_POWER_PINS
+    inout vccd1,	// User area 1 1.8V supply
+    inout vssd1,	// User area 1 digital ground
+`endif
+
+    // Wishbone Slave ports (WB MI A)
+    input wb_clk_i,
+    input wb_rst_i,
+    input wbs_stb_i,
+    input wbs_cyc_i,
+    input wbs_we_i,
+    input [3:0] wbs_sel_i,
+    input [31:0] wbs_dat_i,
+    input [31:0] wbs_adr_i,
+    output wbs_ack_o,
+    output [31:0] wbs_dat_o,
+
+    // Logic Analyzer Signals
+    input  [127:0] la_data_in,
+    output [127:0] la_data_out,
+    input  [127:0] la_oenb,
+
+    // IOs
+    input  [`MPRJ_IO_PADS-1:0] io_in,
+    output [`MPRJ_IO_PADS-1:0] io_out,
+    output [`MPRJ_IO_PADS-1:0] io_oeb,
+
+    // IRQ
+    output [2:0] irq
+);
+    
+
+    wire [`MPRJ_IO_PADS-1:0] io_in;
+    wire [`MPRJ_IO_PADS-1:0] io_out;
+    wire [`MPRJ_IO_PADS-1:0] io_oeb;
+
+      
+    adder4bit myadder(
+        .a(io_in[13:10])
+        .b(io_in[17:14])
+        .sum(io_out[21:18])
+        .co(io_out[22:22])
+    );
+   
+    
+endmodule
+// 4-bit adder
+module adder4bit(a,b,sum,co);
+input [3:0]a,b;
+output [3:0]sum;
+output co;
+fa fa1(a[0],b[0],1'b0,sum[0],c1),
+   fa2(a[1],b[1],c1,sum[1],c2),
+   fa3(a[2],b[2],c2,sum[2],c3),
+   fa4(a[3],b[3],c3,sum[3],co);
+endmodule
+//full adder
+module fa(a,b,cin,sum,co);
+input a,b,cin;
+output sum,co;
+ha ha1(a,b,s1,c1),
+   ha2(cin,s1,sum,c2);
+orgate or1(c1,c2,co);
+endmodule
+// half adder
+module ha(a,b,sum,co);
+input a,b;
+output sum,co;
+assign sum=a^b;
+assign co=a&b;
+endmodule
+//orgate
+module orgate(a,b,y);
+input a,b;
+output y;
+assign y=a|b;
+endmodule
+
+`default_nettype wire
